7031f84468f2ed5ede3df81ed22f08f5cca29543
Shashank krishu <shashankkrishu99@gmail.com>
2023-12-19 15:52:48 +0530

    upgrades

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 // App.jsx
 import React from 'react';
 import DynamicMenu from './Components/DynamicMenu';
-import Carousel from './Components/Carousel';
+import ImageCarousel from './Components/Carousel';
 import './App.css'; // Import your main styling file
+
 
 const App = () => {
   const menuItems = [
@@ -16,18 +17,12 @@ const App = () => {
     // Add more menu items as needed
   ];
 
-  const carouselItems = [
-    { id: 1, imageSrc: 'https://unsplash.com/photos/unpaired-red-nike-sneaker-164_6wVEHfI' },
-  { id: 2, imageSrc: 'https://unsplash.com/photos/a-person-wearing-a-hat-SD9Jyl1xNQ4' },
-  { id: 3, imageSrc: 'https://unsplash.com/photos/black-white-and-red-nike-high-top-sneaker-76w_eDO1u1E' },
-    // Add more carousel items as needed
-  ];
 
   return (
     <div className="app">
-      <h1>Avataar</h1>
+      <h1>Shelby company limited.</h1>
       <DynamicMenu menuItems={menuItems} />
-      <Carousel items = {carouselItems} />
+      <ImageCarousel  />
     </div>
   );
 };
--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -1,35 +1,94 @@
-// Carousel.jsx
+// ImageCarousel.jsx
 import React, { useState, useEffect } from 'react';
 import './Carousel.css';
+import bar from '../asset/bar.png';
+import dot from '../asset/dot.png';
+import LeftArrow from '../asset/arrow_left.png';
+import RightArrow from '../asset/arrow_right.png';
+import firstimge from '../asset/CarouselImages/1.jpg'
 
-const Carousel = ({ items }) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+const ImageCarousel = () => {
+  const images = [
+    {
+      src: 'https://wallpapercosmos.com/w/full/9/b/2/12628-3840x2160-desktop-4k-peaky-blinders-background-image.jpg',
+      alt: 'Image 1',
+      description: 'Description 1',
+    },
+    {
+      src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQr3xhDIYS7Ye4oqokuGOj9D4jmEd4VkqMSg&usqp=CAU',
+      alt: 'Image 2',
+      description: 'Description 2',
+    },
+    {
+      src: 'https://wallpapercosmos.com/w/full/e/2/e/12641-1920x1080-desktop-full-hd-peaky-blinders-background-image.jpg',
+      alt: 'Image 3',
+      description: 'Description 3',
+    },
+    {
+      src:'https://wallpapercave.com/wp/wp1890602.jpg',
+      alt:'Image 4',
+      description:'Description 4'
+    }
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % items.length);
-    }, 3000);
+  ];
 
-    return () => clearInterval(interval);
-  }, [items.length]);
+  const [carouselState, setCarouselState] = useState(3);
+  const [isHidden, setIsHidden] = useState(false);
 
-  const handleItemClick = (index) => {
-    setCurrentIndex(index);
+  const hideDesc = () => {
+    setIsHidden(true);
+    setTimeout(() => {
+      setIsHidden(false);
+    }, 1000);
   };
 
+  const imageFunc = (state) => {
+    setCarouselState(state);
+    hideDesc();
+  };
+
+  const goLeft = () => {
+    setCarouselState((prev) => (prev === 1 ? images.length : prev - 1));
+    hideDesc();
+  };
+
+  const goRight = () => {
+    setCarouselState((prev) => (prev % images.length) + 1);
+    hideDesc();
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(goRight, 3500);
+    return () => clearInterval(intervalId);
+  }, [images.length]);
+
   return (
-    <div className="carousel">
-      {items.map((item, index) => (
-        <div
-          key={item.id}
-          className={`carousel-item ${index === currentIndex ? 'active' : ''}`}
-          onClick={() => handleItemClick(index)}
-        >
-          <img src={item.imageSrc} alt={`Image ${index + 1}`} />
-        </div>
-      ))}
+    <div className='container'>
+      <div className={`carousel state-${carouselState}`}>
+        {images.map((image, index) => (
+          <img
+            key={index}
+            src={image.src}
+            alt={image.alt}
+            onClick={() => imageFunc(index + 1)}
+          />
+        ))}
+        {!isHidden && <div className='desc'>{images[carouselState - 1].description}</div>}
+      </div>
+      <div className='nav'>
+        <img src={LeftArrow} alt='Left' onClick={goLeft} />
+        {Array.from({ length: images.length }).map((_, index) => (
+          <img
+            key={index}
+            src={index === carouselState - 1 ? bar : dot}
+            alt='State'
+            onClick={() => imageFunc(index + 1)}
+          />
+        ))}
+        <img src={RightArrow} alt='Right' onClick={goRight} />
+      </div>
     </div>
   );
 };
 
-export default Carousel;
+export default ImageCarousel;
